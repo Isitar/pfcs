10ec95e62a52bac65747803f2697ea368ca8726e
Pascal Lüscher <pascal.luescher@asinfotrack.ch>
2018-01-09 11:01:14 +0100

    Kugelstoss erweitert

--- src/ch/fhnw/pfcs/Stoss2D.java
+++ src/ch/fhnw/pfcs/Stoss2D.java
@@ -1,4 +1,5 @@
 package ch.fhnw.pfcs;
+
 //  -------------   2D-Stoss (Curling)  -------------------
 import java.awt.*;
 import java.awt.event.*;
@@ -7,189 +8,247 @@ import com.jogamp.opengl.awt.*;
 import com.jogamp.opengl.util.*;
 import ch.fhnw.util.math.*;
 
-public class Stoss2D
-       implements WindowListener, GLEventListener, KeyListener
-{
+public class Stoss2D implements WindowListener, GLEventListener, KeyListener {
 
-    //  ---------  globale Daten  ---------------------------
+	// --------- globale Daten ---------------------------
 
-    String windowTitle = "Zweidimensionaler Stoss";
-    int windowWidth = 800;
-    int windowHeight = 600;
-    String vShader = MyShaders.vShader1;                 // Vertex-Shader
-    String fShader = MyShaders.fShader0;                 // Fragment-Shader
-    int maxVerts = 2048;                                 // max. Anzahl Vertices im Vertex-Array
-    GLCanvas canvas;                                     // OpenGL Window
-    MyGLBase1 mygl;                                      // eigene OpenGL-Basisfunktionen
-    Mat4 M = Mat4.ID;                                    // Transf.Matrix
-    Mat4 P = Mat4.ID;                                    // Proj. Matrix
-    double xleft = -2, xright = 2;                       // ViewingVol
-    double ybottom, ytop;
-    double znear = -100, zfar = 1000;
+	String windowTitle = "Zweidimensionaler Stoss";
+	int windowWidth = 800;
+	int windowHeight = 600;
+	String vShader = MyShaders.vShader1; // Vertex-Shader
+	String fShader = MyShaders.fShader0; // Fragment-Shader
+	int maxVerts = 2048; // max. Anzahl Vertices im Vertex-Array
+	GLCanvas canvas; // OpenGL Window
+	MyGLBase1 mygl; // eigene OpenGL-Basisfunktionen
+	Mat4 M = Mat4.ID; // Transf.Matrix
+	Mat4 P = Mat4.ID; // Proj. Matrix
+	double xleft = -2, xright = 2; // ViewingVol
+	double ybottom, ytop;
+	double znear = -100, zfar = 1000;
 
-    double dt = 0.1;                                     // Zeitschritt
-    boolean stopped = false;
+	double dt = 0.01; // Zeitschritt
+	boolean stopped = false;
 
+	// ------- Hilfsklasse -------------
+	class Kugel {
+		double m, r, xm, ym, vx, vy;
 
-    //  -------  Hilfsklasse  -------------
-    class Kugel
-    {
-       double m,r,xm,ym,vx,vy;
+		/**
+		 * 
+		 * @param r
+		 *            Radius
+		 * @param m
+		 *            Masse
+		 * @param xm
+		 *            Mitteplunkt X
+		 * @param ym
+		 *            Mittelpunkt Y
+		 * @param vx
+		 *            Geschwindigkeit X
+		 * @param vy
+		 *            Geschwindigkeit Y
+		 */
+		public Kugel(double r, double m, double xm, double ym, double vx, double vy) {
+			this.xm = xm;
+			this.ym = ym;
+			this.vx = vx;
+			this.vy = vy;
+			this.r = r;
+			this.m = m;
+		}
 
-       public Kugel(double r, double m,                  // Radius, Masse
-                    double xm, double ym,                // Mittelpunktkoord.
-                    double vx, double vy)                // Geschwindigkeit
-       {  this.xm = xm;
-          this.ym = ym;
-          this.vx = vx;
-          this.vy = vy;
-          this.r = r;
-          this.m = m;
-      }
+		public void zeichne(GL3 gl) // Kreis zeichnen
+		{
+			int nPkte = 20;
+			double phi = 2 * Math.PI / (nPkte - 1);
+			double x, y;
+			mygl.rewindBuffer(gl);
+			mygl.putVertex((float) xm, (float) ym, 0);
+			for (int i = 0; i < nPkte; i++) {
+				x = xm + r * Math.cos(i * phi);
+				y = ym + r * Math.sin(i * phi);
+				mygl.putVertex((float) x, (float) y, 0);
+			}
+			mygl.copyBuffer(gl);
+			mygl.drawArrays(gl, GL3.GL_TRIANGLE_FAN);
+		}
 
-      public void zeichne(GL3 gl)                        // Kreis zeichnen
-      {  int nPkte = 20;
-         double phi = 2*Math.PI / (nPkte-1);
-         double x,y;
-         mygl.rewindBuffer(gl);
-         mygl.putVertex((float)xm,(float)ym,0);
-         for (int i=0; i < nPkte; i++)
-         {  x = xm + r * Math.cos(i*phi);
-            y = ym + r * Math.sin(i*phi);
-            mygl.putVertex((float)x,(float)y, 0);
-         }
-         mygl.copyBuffer(gl);
-         mygl.drawArrays(gl,GL3.GL_TRIANGLE_FAN);
-      }
+		public void move(double dt) // Bewegungsschritt
+		{
+			xm += vx * dt;
+			ym += vy * dt;
+		}
 
-      public void move(double dt)                        // Bewegungsschritt
-      {  xm += vx*dt;
-         ym += vy*dt;
-      }
+		public Vec3 getVVec() {
+			return new Vec3(vx, vy, 0);
+		}
+		
+		public void setVVec(Vec3 v) {
+			this.vx = v.x;
+			this.vy = v.y;
+		}
+		
+		public void setVVec(double[] d) {
+			this.vx = d[0];
+			this.vy = d[1];
+		}
+	}
 
-    }
+	Kugel k1 = new Kugel(0.1, 0.02, 0, 0, 0, 0);
+	Kugel k2 = new Kugel(0.05, 0.01, xleft, 0.1, 1, 0);
+	// --------- Methoden ----------------------------------
 
+	public void stoss(Kugel k1, Kugel k2, double stossZahl) {
+		Vec3 v1 = k1.getVVec();
+		Vec3 v2 = k2.getVVec();
+		Vec3 n = new Vec3(k2.xm - k1.xm, k2.ym - k1.ym, 0).normalize();
+		double v1n = v1.dot(n);
+		double v2n = v2.dot(n);
+		double[] newV = stoss1D(k1.m,v1n,k2.m,v2n,stossZahl);
+		Vec3 vv1 = n.scale((float)newV[0]);
+		
+	}
 
+	/**
+	 * 
+	 * @param m1
+	 * @param v1
+	 * @param m2
+	 * @param v2
+	 * @param k
+	 * @return double[] {v1',v2'}
+	 */
+	private double[] stoss1D(double m1, double v1, double m2, double v2, double k) {
+		return new double[] { (m1 * v1 + m2 * v2 - (v1 - v2) * m2 * k) / (m1 + m2),
+				(m1 * v1 + m2 * v2 - (v2 - v1) * m1 * k) / (m1 + m2) };
+	}
 
-    Kugel k1 = new Kugel(0.1, 0.02, 0, 0, 0, 0);
+	public void zeichneAchsen(GL3 gl, float a, float b) // Koordinatenachsen zeichnen
+	{
+		mygl.rewindBuffer(gl);
+		mygl.putVertex(-a, 0, 0);
+		mygl.putVertex(a, 0, 0);
+		mygl.putVertex(0, -b, 0);
+		mygl.putVertex(0, b, 0);
+		mygl.copyBuffer(gl);
+		mygl.drawArrays(gl, GL3.GL_LINES);
+	}
 
+	public Stoss2D() // Konstruktor
+	{
+		createFrame();
+	}
 
-    //  ---------  Methoden  ----------------------------------
+	void createFrame() // Fenster erzeugen
+	{
+		Frame f = new Frame(windowTitle);
+		f.setSize(windowWidth, windowHeight);
+		f.addWindowListener(this);
+		GLProfile glp = GLProfile.get(GLProfile.GL3);
+		GLCapabilities glCaps = new GLCapabilities(glp);
+		canvas = new GLCanvas(glCaps);
+		canvas.addGLEventListener(this);
+		f.add(canvas);
+		f.setVisible(true);
+		f.addKeyListener(this);
+		canvas.addKeyListener(this);
+	};
 
+	// ---------- OpenGL-Events ---------------------------
 
-    public void zeichneAchsen(GL3 gl, float a, float b)      // Koordinatenachsen zeichnen
-    {  mygl.rewindBuffer(gl);
-       mygl.putVertex(-a,0,0);
-       mygl.putVertex(a,0,0);
-       mygl.putVertex(0,-b,0);
-       mygl.putVertex(0,b,0);
-       mygl.copyBuffer(gl);
-       mygl.drawArrays(gl, GL3.GL_LINES);
-    }
+	@Override
+	public void init(GLAutoDrawable drawable) // Initialisierung
+	{
+		GL3 gl = drawable.getGL().getGL3();
+		System.out.println("OpenGl Version: " + gl.glGetString(gl.GL_VERSION));
+		System.out.println("Shading Language: " + gl.glGetString(gl.GL_SHADING_LANGUAGE_VERSION));
+		System.out.println();
+		gl.glClearColor(0.2f, 0.4f, 1.0f, 1); // Hintergrundfarbe
+		int programId = MyShaders.initShaders(gl, vShader, fShader); // Compile/Link Shader-Programme
+		mygl = new MyGLBase1(gl, programId, maxVerts); // eigene OpenGL Basis-Funktionen
+		FPSAnimator anim = new FPSAnimator(canvas, 200, true); // Animations-Thread, 200 Frames/sek
+		anim.start();
+	}
 
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		GL3 gl = drawable.getGL().getGL3();
+		gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
+		M = Mat4.ID;
+		mygl.setM(gl, M);
+		mygl.setColor(0.6f, 0.6f, 0.6f);
+		zeichneAchsen(gl, 10, 10);
 
-    public Stoss2D()                                          // Konstruktor
-    { createFrame();
-    }
+		// ------ Kugel zeichnen -----
+		mygl.setColor(1f, 0.9f, 0.2f);
+		k1.zeichne(gl);
+		k2.zeichne(gl);
+		k1.move(dt);
+		k2.move(dt);
+	}
 
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+		GL3 gl = drawable.getGL().getGL3();
+		// Set the viewport to be the entire window
+		gl.glViewport(0, 0, width, height);
+		double aspect = (double) height / width;
+		ybottom = aspect * xleft;
+		ytop = aspect * xright;
+		P = Mat4.ortho((float) xleft, (float) xright, (float) ybottom, (float) ytop, (float) znear, (float) zfar);
+		mygl.setP(gl, P);
+	}
 
-    void createFrame()                                       // Fenster erzeugen
-    {  Frame f = new Frame(windowTitle);
-       f.setSize(windowWidth, windowHeight);
-       f.addWindowListener(this);
-       GLProfile glp = GLProfile.get(GLProfile.GL3);
-       GLCapabilities glCaps = new GLCapabilities(glp);
-       canvas = new GLCanvas(glCaps);
-       canvas.addGLEventListener(this);
-       f.add(canvas);
-       f.setVisible(true);
-       f.addKeyListener(this);
-       canvas.addKeyListener(this);
-    };
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+	} // not needed
 
+	// ----------- main-Methode ---------------------------
 
-    //  ----------  OpenGL-Events   ---------------------------
+	public static void main(String[] args) {
+		new Stoss2D();
+	}
 
-    @Override
-    public void init(GLAutoDrawable drawable)                      //  Initialisierung
-    {  GL3 gl = drawable.getGL().getGL3();
-       System.out.println("OpenGl Version: " + gl.glGetString(gl.GL_VERSION));
-       System.out.println("Shading Language: " + gl.glGetString(gl.GL_SHADING_LANGUAGE_VERSION));
-       System.out.println();
-       gl.glClearColor(0.2f,0.4f,1.0f,1);                                   // Hintergrundfarbe
-       int programId = MyShaders.initShaders(gl,vShader,fShader);  // Compile/Link Shader-Programme
-       mygl = new MyGLBase1(gl, programId, maxVerts);              // eigene OpenGL Basis-Funktionen
-       FPSAnimator anim = new FPSAnimator(canvas, 200, true);      // Animations-Thread, 200 Frames/sek
-       anim.start();
-    }
+	// --------- Window-Events --------------------
 
+	public void windowClosing(WindowEvent e) {
+		System.out.println("closing window");
+		System.exit(0);
+	}
 
-    @Override
-    public void display(GLAutoDrawable drawable)
-    { GL3 gl = drawable.getGL().getGL3();
-      gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
-      M = Mat4.ID;
-      mygl.setM(gl, M);
-      mygl.setColor(0.6f,0.6f,0.6f);
-      zeichneAchsen(gl, 10, 10);
+	public void windowActivated(WindowEvent e) {
+	}
 
-      // ------  Kugel zeichnen -----
-      mygl.setColor(1f, 0.9f, 0.2f);
-      k1.zeichne(gl);
-    }
+	public void windowClosed(WindowEvent e) {
+	}
 
+	public void windowDeactivated(WindowEvent e) {
+	}
 
-    @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y,
-                        int width, int height)
-    {  GL3 gl = drawable.getGL().getGL3();
-       // Set the viewport to be the entire window
-       gl.glViewport(0, 0, width, height);
-       double aspect = (double)height / width;
-       ybottom = aspect * xleft;
-       ytop = aspect * xright;
-       P = Mat4.ortho((float)xleft, (float)xright,
-                  (float)ybottom, (float)ytop,
-                  (float)znear, (float)zfar);
-       mygl.setP(gl, P);
-   }
+	public void windowDeiconified(WindowEvent e) {
+	}
 
+	public void windowIconified(WindowEvent e) {
+	}
 
-    @Override
-    public void dispose(GLAutoDrawable drawable)  { }                  // not needed
+	public void windowOpened(WindowEvent e) {
+	}
 
-    //  -----------  main-Methode  ---------------------------
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+	}
 
-    public static void main(String[] args)
-    { new Stoss2D();
-    }
+	public void keyReleased(KeyEvent e) {
+	}
 
-    //  ---------  Window-Events  --------------------
-
-    public void windowClosing(WindowEvent e)
-    {   System.out.println("closing window");
-        System.exit(0);
-    }
-    public void windowActivated(WindowEvent e) {  }
-    public void windowClosed(WindowEvent e) {  }
-    public void windowDeactivated(WindowEvent e) {  }
-    public void windowDeiconified(WindowEvent e) {  }
-    public void windowIconified(WindowEvent e) {  }
-    public void windowOpened(WindowEvent e) {  }
-
-    public void keyPressed(KeyEvent e)
-    { int key = e.getKeyCode();
-    }
-
-    public void keyReleased(KeyEvent e) { }
-
-    public void keyTyped(KeyEvent e)
-    {  char key = e.getKeyChar();
-       switch ( key )
-       {  case 's':
-          case 'S': stopped = !stopped;
-                    break;
-      }
-    }
+	public void keyTyped(KeyEvent e) {
+		char key = e.getKeyChar();
+		switch (key) {
+		case 's':
+		case 'S':
+			stopped = !stopped;
+			break;
+		}
+	}
 
 }
